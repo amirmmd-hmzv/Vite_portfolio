3dc39945599e56a13c1de287738839500918fea8
amirmmd_hmzv <amirhamzavi44@gmail.com>
2025-06-11 23:18:53 +0330

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,44 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import compression from "vite-plugin-compression";
+import viteImagemin from "vite-plugin-imagemin";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ["**/*.PNG"],
+  assetsInclude: ["**/*.png", "**/*.PNG"],
   base: "./",
-	plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 10240,
+      verbose: true,
+      deleteOriginFile: false,
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 70,
+      },
+      pngquant: {
+        quality: [0.65, 0.8],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          { name: "removeViewBox", active: false },
+          { name: "cleanupIDs", active: true },
+        ],
+      },
+      webp: {
+        quality: 75,
+      },
+    }),
+  ],
 });
